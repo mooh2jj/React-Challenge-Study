1fa5294a38b8d80535362f397a0f2cf43bbd3ae8
Mo-Greene <13blueboy13@daum.net>
2023-11-06 12:14:55 +0900

    feat: SpringBoot Setting

---
+++ 장현석/Back/react-challenge/src/main/java/com/mo/reactchallenge/entity/Board.java
@@ -0,0 +1,12 @@
+package com.mo.reactchallenge.entity;/**
+ * packageName    : com.mo.reactchallenge.entity
+ * fileName       : Board
+ * author         : Jang Hyun Suk
+ * date           : 11/6/23
+ * description    : 
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 11/6/23       Jang Hyun Suk       최초 생성
+ */public class Board {
+}
---
+++ 장현석/Back/react-challenge/src/main/java/com/mo/reactchallenge/entity/core/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.mo.reactchallenge.entity.core;/**
+ * packageName    : com.mo.reactchallenge.entity.core
+ * fileName       : BaseEntity
+ * author         : Jang Hyun Suk
+ * date           : 11/6/23
+ * description    : 
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 11/6/23       Jang Hyun Suk       최초 생성
+ */public class BaseEntity {
+}
